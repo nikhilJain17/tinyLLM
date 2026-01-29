1d1023442befd4277518ae5d1e7d78e04522d836
Nikhil Jain <nikhil.jain0987@gmail.com>
2026-01-28 20:45:13 -0800

    Fix bug in loading data and parse magic number

--- src/weightloader/weightloader.cpp
+++ src/weightloader/weightloader.cpp
@@ -1,10 +1,11 @@
 #include "weightloader.hpp"
-
+#include <iomanip>
 WeightLoader::WeightLoader(const std::string &filepath) : filepath(filepath) {
   this->buffer = std::vector<uint8_t>();
   // TODO: perf
   this->buffer.reserve(CHUNK);
   this->load_fully_resident();
+  DEBUG_LOG("Magic number is ", this->parse_magic_number() == 1 ? "VALID" : "NOT VALID");
 }
 
 void WeightLoader::load_fully_resident() {
@@ -14,13 +15,18 @@ void WeightLoader::load_fully_resident() {
     DEBUG_LOG("Error opening file! ", this->filepath);
     exit(-1);
   }
-  ssize_t bytes_read;
+  ssize_t bytes_read = 0;
   ssize_t total_bytes_read = 0;
-  while ((bytes_read = read(fd, this->buffer.data(), CHUNK)) > 0) {
+  while ((bytes_read = read(fd, this->buffer.data() + bytes_read, CHUNK)) > 0) {
     DEBUG_LOG("Read ", bytes_read, " bytes, with chunk size ", CHUNK,
               " bytes.");
     total_bytes_read += bytes_read;
   }
   DEBUG_LOG("Successfully loaded weights from ", this->filepath, " for ",
             total_bytes_read, " bytes.");
+}
+
+bool WeightLoader::parse_magic_number() {
+    return this->buffer[0] == 0x47 && this->buffer[1] == 0x47
+        && this->buffer[2] == 0x55 && this->buffer[3] == 0x46;
 }

655341627e4768b92f711aef6f3d31fa405e70ab
Nikhil Jain <nikhil.jain0987@gmail.com>
2026-01-28 20:23:34 -0800

    Load in weights fully resident into memory

--- src/weightloader/weightloader.cpp
+++ src/weightloader/weightloader.cpp
@@ -4,8 +4,21 @@ WeightLoader::WeightLoader(const std::string & filepath) : filepath(filepath) {
     this->buffer = std::vector<uint8_t>();
     // TODO: perf
     this->buffer.reserve(CHUNK);
+    this->load_fully_resident();
 }
 
-bool WeightLoader::load_fully_resident() {
-    
+void WeightLoader::load_fully_resident() {
+    DEBUG_LOG("Loading weights from ", this->filepath);
+    int fd = open(this->filepath.c_str(), O_RDONLY);
+    if (fd == -1) {
+        DEBUG_LOG("Error opening file! ", this->filepath);
+        exit(-1);
+    }
+    ssize_t bytes_read;
+    ssize_t total_bytes_read = 0;
+    while ((bytes_read = read(fd, this->buffer.data(), CHUNK)) > 0) {
+        DEBUG_LOG("Read ", bytes_read, " bytes, with chunk size ", CHUNK, " bytes.");
+        total_bytes_read += bytes_read;
+    }
+    DEBUG_LOG("Successfully loaded weights from ", this->filepath, " for ", total_bytes_read, " bytes.");
 }
--- src/weightloader/weightloader.hpp
+++ src/weightloader/weightloader.hpp
@@ -4,23 +4,36 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <unistd.h>
+#include <fcntl.h>
 
 #define CHUNK 16777216
+
+typedef struct {
+    int num_tensors;
+    
+} Metadata;
+
+typedef struct {
+    std::vector<int> dim;
+    std::vector<uint8_t> data;
+} Tensor;
 
 class WeightLoader
 {
 private:
     std::vector<uint8_t> buffer;
     const std::string filepath;
-    bool check_magic_number();
+    bool parse_magic_number();
     int parse_gguf_version();
-
+    // If the GPU has enough VRAM, load all tensors
+    void load_fully_resident();
+    // Otherwise, stream them to GPU
+    bool stream_to_gpu();
 
 public:
     WeightLoader(const std::string & filepath);
-    // If the GPU has enough VRAM, load all tensors
-    bool load_fully_resident();
-    // Otherwise, stream them to GPU
-    bool stream_to_gpu();
+    Metadata get_metadata();
+    Tensor fetch_tensor(std::string& tensor_name);
 };
 
